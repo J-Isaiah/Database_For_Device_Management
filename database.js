8b288984dae1ce41c4d8765196b2e8b585ec8610
Isaiah <Isaiah.Jacobsen1@gmail.com>
2024-03-13 18:58:41 -0500

    Fixed bug that caused everything to not work at all... very cool!

--- database.js
+++ database.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 
 
 var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host:process.env.MYSQL_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE
 })
 
 // View All Devices
