a43c1538187d2f0b5a156a088418d1a8a85f8dae
Isaiah <isaiahschooluno@gmail.com>
2024-03-28 16:02:41 -0500

    Finished add device function, both functions are still in the code one commented out, just incase any issues arrise with the original function.

--- database.js
+++ database.js
@@ -54,61 +54,133 @@ function allDevices() {
 }
 
 
+// async function addDevice(location_school, electrocardiogram, inertial_measurement_unit, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response, micro_controller_number, real_time_clock_model_number, info_about_data_storage, firmware_version, date_installed, model_number, device_status, date_deployed) {
+//     connection.beginTransaction(async function (err) {
+//         if (err) {
+//             throw err
+//         }
+//
+//         let device_fkey = []
+//         let queries = [{
+//             q: 'INSERT INTO hardware_profile (microcontroller_model_number, real_time_clock_model_number, info_about_data_storage) VALUES (?,?,?)',
+//             v: [micro_controller_number, real_time_clock_model_number, info_about_data_storage]
+//         }, {
+//             q: 'INSERT INTO sensors (electrocardiogram, IMU, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response) VALUES (?,?,?,?,?,?,?)',
+//             v: [electrocardiogram, inertial_measurement_unit, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response]
+//         }, {
+//             q: 'INSERT INTO firmware (firmware_version, date_installed) VALUES (?,?)',
+//             v: [firmware_version, date_installed]
+//         }, {
+//             q: 'INSERT INTO location_school (university_name) values (?)', v: [location_school]
+//
+//         },]
+//
+//         await Promise.all(queries.map(query => {
+//             return new Promise((resolve, reject) => {
+//                 console.log((query.q))
+//                 connection.query(query.q, query.v, function (err, result) {
+//                     if (err) {
+//                         console.error('err executing query', err)
+//                         reject(err)
+//                         connection.rollback(function () {
+//                             console.log(err)
+//                         })
+//
+//                     } else {
+//                         device_fkey.push(result.insertId)
+//                         console.log(device_fkey)
+//                         resolve()
+//
+//
+//                     }
+//
+//                 })
+//             })
+//         }))
+//
+//         let deviceQ = 'INSERT INTO device (model_number, device_status, date_deployed, Hardware_id, sensor_id, firmware_id,location_id) values (?,?,?,?,?,?,?)'
+//         let deviceV = [model_number, device_status, date_deployed, ...device_fkey]
+//         connection.query(deviceQ, deviceV, function (err, result) {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 console.log('a new device was added successfully')
+//                 connection.commit()
+//                 console.log('committed to db')
+//             }
+//         })
+//     })
+// }
+
 async function addDevice(location_school, electrocardiogram, inertial_measurement_unit, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response, micro_controller_number, real_time_clock_model_number, info_about_data_storage, firmware_version, date_installed, model_number, device_status, date_deployed) {
     connection.beginTransaction(async function (err) {
         if (err) {
-            throw err
+            throw err;
         }
 
-        let device_fkey = []
-        let queries = [{
-            q: 'INSERT INTO hardware_profile (microcontroller_model_number, real_time_clock_model_number, info_about_data_storage) VALUES (?,?,?)',
-            v: [micro_controller_number, real_time_clock_model_number, info_about_data_storage]
-        }, {
-            q: 'INSERT INTO sensors (electrocardiogram, IMU, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response) VALUES (?,?,?,?,?,?,?)',
-            v: [electrocardiogram, inertial_measurement_unit, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response]
-        }, {
-            q: 'INSERT INTO firmware (firmware_version, date_installed) VALUES (?,?)',
-            v: [firmware_version, date_installed]
-        }, {
-            q: 'INSERT INTO location_school (university_name) values (?)', v: [location_school]
+        try {
+            let device_fkey = [];
+            let queries = [{
+                q: 'INSERT INTO hardware_profile (microcontroller_model_number, real_time_clock_model_number, info_about_data_storage) VALUES (?,?,?)',
+                v: [micro_controller_number, real_time_clock_model_number, info_about_data_storage]
+            }, {
+                q: 'INSERT INTO sensors (electrocardiogram, IMU, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response) VALUES (?,?,?,?,?,?,?)',
+                v: [electrocardiogram, inertial_measurement_unit, optical_pulse_oximeter, microphone, temperature_sensor, electronic_nose, galvanic_skin_response]
+            }, {
+                q: 'INSERT INTO firmware (firmware_version, date_installed) VALUES (?,?)',
+                v: [firmware_version, date_installed]
+            }, {
+                q: 'INSERT INTO location_school (university_name) values (?)', v: [location_school]
 
-        },]
+            },]
 
-        await Promise.all(queries.map(query => {
-            return new Promise((resolve, reject) => {
-                console.log((query.q))
-                connection.query(query.q, query.v, function (err, result) {
+
+            await Promise.all(queries.map(async (query) => {
+                const result = await new Promise((resolve, reject) => {
+                    connection.query(query.q, query.v, function (err, result) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        resolve(result);
+                    });
+                });
+                device_fkey.push(result.insertId);
+            }));
+
+
+            let deviceQ = 'INSERT INTO device (model_number, device_status, date_deployed, Hardware_id, sensor_id, firmware_id,location_id) values (?,?,?,?,?,?,?)'
+            let deviceV = [model_number, device_status, date_deployed, ...device_fkey]
+
+            await new Promise((resolve, reject) => {
+                connection.query(deviceQ, deviceV, function (err, result) {
                     if (err) {
-                        console.error('err executing query', err)
-                        reject(err)
-                        connection.rollback(function () {
-                            throw err
-                        })
-
-                    } else {
-                        device_fkey.push(result.insertId)
-                        console.log(device_fkey)
-                        resolve()
-
-
+                        return reject(err);
                     }
+                    resolve();
+                });
+            });
 
-                })
-            })
-        }))
 
-        let deviceQ = 'INSERT INTO device (model_number, device_status, date_deployed, Hardware_id, sensor_id, firmware_id,location_id) values (?,?,?,?,?,?,?)'
-        let deviceV = [model_number, device_status, date_deployed, ...device_fkey]
-        connection.query(deviceQ, deviceV, function (err, result) {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log('a new device was added successfully')
-                connection.commit(console.log('commited to db'))
-            }
-        })
-    })
+            await new Promise((resolve, reject) => {
+                connection.commit((err) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    console.log('Transaction committed successfully.');
+                    resolve();
+                });
+            });
+
+        } catch (error) {
+            console.error('Transaction failed, rolling back.', error);
+            await new Promise((resolve, reject) => {
+                connection.rollback(() => {
+                    resolve(); // Always resolve after a rollback attempt
+                });
+            });
+            throw error;
+        }
+    });
 }
 
 module.exports = {addDevice, allDevices}
